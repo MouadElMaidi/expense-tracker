988e6fb24f695a0e66e3f9eedb8b48f01047113e
MouadElMaidi <mouadlmaidi@gmail.com>
2023-08-05 12:55:30 +0100

    Added a chart that show the expenses of every month of the year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 
-import Card from "../UI/Card";
 import "./Expenses.css";
+
+import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -23,6 +25,7 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
           filteredYear={filteredYear}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
